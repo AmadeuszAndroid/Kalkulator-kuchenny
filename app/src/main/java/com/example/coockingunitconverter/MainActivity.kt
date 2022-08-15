9cae068a38d90626f91adcdace291146fc8e9f9d
Amadeusz Lisiecki <amadeusz.lisiecki.1993@gmail.com>
2022-08-15 16:19:04 +0200

    Konwertowanie jednostek.

--- app/src/main/java/com/example/coockingunitconverter/MainActivity.kt
+++ app/src/main/java/com/example/coockingunitconverter/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.coockingunitconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.coockingunitconverter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.convert.setOnClickListener { convertMeasures() }
+    }
+
+    private fun convertMeasures() {
+        val stringInTextFiled = binding.valueToConvert.text.toString()
+        val valueToConvert = stringInTextFiled.toDouble()
+        val reverseConvert = binding.reverseCalculator.isChecked
+
+        val selectedTablewareId = binding.tableware.checkedRadioButtonId
+
+        val result: Double
+        val multiplier = when (selectedTablewareId) {
+            R.id.glasses -> 250
+            R.id.spoons -> 15
+            else -> 5
+        }
+        result = if (reverseConvert) {
+            valueToConvert * multiplier
+        } else {
+            valueToConvert / multiplier
+        }
+        binding.result.text = result.toString()
     }
 }
